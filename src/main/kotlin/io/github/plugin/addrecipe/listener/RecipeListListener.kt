da4260c1fd148b1c2c529f7afd89cf6b18e070fd
RANK <81941006+AtSignRANK@users.noreply.github.com>
2021-07-16 21:11:39 +0900

    developing

--- src/main/kotlin/io/github/plugin/addrecipe/listener/RecipeListListener.kt
+++ src/main/kotlin/io/github/plugin/addrecipe/listener/RecipeListListener.kt
@@ -1,6 +1,7 @@
 package io.github.plugin.addrecipe.listener
 
 import io.github.plugin.addrecipe.AddRecipe
+import net.kyori.adventure.text.Component
 import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -11,14 +12,27 @@ class RecipeListListener(private val plugin: AddRecipe): Listener {
     fun onInventoryClick(event: InventoryClickEvent) {
         val player = event.whoClicked
         val inventory = event.inventory
-        if (event.view.title() == plugin.recipelist) {
+        if (event.view.title() == plugin.recipelist && event.clickedInventory != player.inventory) {
+            event.isCancelled = true
             if (plugin.recipelistclicknotable.contains(event.slot)) {
-                if (event.slot == 45) {
-                    val page = plugin.getIntInString(inventory.getItem(47)?.displayName()?.insertion()!!,
-                        "${ChatColor.GREEN}현재 페이지 : ", 1, 10)
+                val page = plugin.getIntInString(inventory.getItem(47)?.displayName()?.insertion()!!,
+                    "${ChatColor.GREEN}현재 페이지 : ", 1, 10)
+                if (event.slot < 45) {
                     val number = (page - 1) * 45 + event.slot
                     val newInventory = plugin.getRecipe(number)
                     player.openInventory(newInventory)
+                } else if (event.slot == 45) {
+                    if (page == 1) {
+                        player.sendMessage(Component.text("${ChatColor.RED}이전 페이지가 존재하지 않습니다!"))
+                        return
+                    }
+                    player.openInventory(plugin.recipeList(page - 1))
+                } else if (event.slot == 53) {
+                    if (page == 10) {
+                        player.sendMessage(Component.text("${ChatColor.RED}다음 페이지가 존재하지 않습니다!"))
+                        return
+                    }
+                    player.openInventory(plugin.recipeList(page + 1))
                 }
             }
         }
